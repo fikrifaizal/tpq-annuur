8686098f1a47c1dd4f4b5e964afd022d29cd5187
fikrifaizal <fikrifaizal40@gmail.com>
2022-07-20 14:49:41 +0700

    fix responsive navbar

--- user/admin/layout/sidebar.php
+++ user/admin/layout/sidebar.php
@@ -51,24 +51,27 @@ $role = explode(" ", $_SESSION["role"]);
           <div class="d-flex">
             <ul class="navbar-nav">
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle topbar-text" id="userDropdown" role="button"
-                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <span class="mr-2 d-none d-lg-inline me-2"><?= ucfirst(strtolower($name['0']))?></span>
+                <a class="nav-link dropdown-toggle dropdown-user" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="mr-2 d-lg-inline me-2"><?= ucfirst(strtolower($name['0']))?></span>
                   <img class="img-profile rounded-circle" src="\assets\image\user.png">
                 </a>
 
                 <!-- Dropdown User Information -->
-                <div class="dropdown-menu dropdown-menu-end dropdown-margin-top" aria-labelledby="userDropdown">
-                  <a class="dropdown-item" href="/user/admin/pengaturan/pengaturan.php">
-                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Pengaturan
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="/user/auth/logout.php" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Keluar
-                  </a>
-                </div>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-margin-top fade-down" aria-labelledby="userDropdown">
+                  <li>
+                    <a class="dropdown-item" href="/user/admin/pengaturan/pengaturan.php">
+                      <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                      <span>Pengaturan</span>
+                    </a>
+                  </li>
+                  <li><div class="dropdown-divider"></div></li>
+                  <li>
+                    <a class="dropdown-item" href="/user/auth/logout.php" data-toggle="modal" data-target="#logoutModal">
+                      <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                      <span>Keluar</span>
+                    </a>
+                  </li>
+                </ul>
               </li>
             </ul>
           </div>
