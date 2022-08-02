f7a1f2e1fe3a9a3b8329326c916766cfe2f6a9cf
fikrifaizal <fikrifaizal40@gmail.com>
2022-08-02 13:36:45 +0700

    add new form with input file

---
+++ user/admin/santri/data-santri/action/tambah/with-file.php
@@ -0,0 +1,96 @@
+<?php
+require_once('../../../../../vendor/autoload.php');
+require_once('../../../../config.php');
+require_once('../../../akses.php');
+
+// form tambah data
+if(isset($_POST['tambahfile'])) {
+  $fileName	= $_FILES['excel']['tmp_name'];
+
+  // get latest Nomor Induk Santri in database
+  $queryCheck = "SELECT nis FROM santri WHERE nis LIKE '%$getYear%' ORDER BY nis DESC LIMIT 1";
+  $resultCheck = mysqli_query($conn, $queryCheck);
+  $dataCheck = mysqli_fetch_array($resultCheck, MYSQLI_ASSOC);
+  $setNIS = $dataCheck['nis'];
+
+  // Create a new Xls Reader
+  $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+
+  // Load $inputFileName to a Spreadsheet Object
+  $spreadsheet = $reader->load($fileName);
+  $sheet = $spreadsheet->getActiveSheet()->toArray();
+  
+  foreach($sheet as $x => $excel) {
+    // skip table head
+    if($x == 0) {
+      continue;
+    }
+
+    $setNIS = $setNIS+1;
+    $data = "'";
+    // set data
+
+    $data .= $setNIS."','";
+    $data .= addslashes($excel[1])."','";
+    $data .= addslashes($excel[2])."','";
+    $data .= addslashes($excel[3])."','";
+    $data .= $excel[4]."','";
+    $data .= $excel[5]."','";
+    $data .= $excel[6]."','";
+    $data .= addslashes($excel[7])."','";
+    $data .= addslashes($excel[8])."','";
+    $data .= addslashes($excel[9])."','";
+    $data .= addslashes($excel[10])."','";
+    $data .= "0".$excel[11]."','";
+    $data .= addslashes($excel[12])."','";
+    $data .= $excel[13]."','";
+    $data .= "AKTIF"."','";
+    $data .= date("Y-m-d")."'";
+
+    $query = "INSERT INTO santri(`nis`, `nama_lengkap`, `panggilan`, `tempat_lahir`, `tgl_lahir`, `jenjang_sekolah`, `kelas`,
+              `nama_bapak`, `pekerjaan_bapak`,`nama_ibu`, `pekerjaan_ibu`, `no_telp_ortu`, `alamat_ortu`, `infak_bulanan`, `status`, `tgl_daftar`)
+              VALUES ($data)";
+    $result = mysqli_query($conn, $query);
+  }
+  echo("<script>location.href = '/user/admin/santri/data-santri/santri.php';</script>");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <!-- form input -->
+  <form method="post" enctype="multipart/form-data">
+
+    <!-- File Excel -->
+    <div class="form-group row">
+      <label for="excel" class="col-sm-2 col-form-label">File Excel</label>
+      <div class="col-sm-10">
+        <div class="input-group">
+          <input class="form-control" name="excel" id="excel" type="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" required>
+          <a href="/assets/berkas/format.xlsx" class="btn btn-primary" target="_blank">
+            <span>Unduh Format Excel</span>
+          </a>
+        </div>
+        <small class="form-text text-muted">
+          * Tipe File: xlsx Ukuran Maksimal: 2MB
+        </small>
+      </div>
+    </div><br>
+
+    <!-- Button -->
+    <div class="form-group row">
+      <label for="button" class="col-sm-2 col-form-label"></label>
+      <div class="col-sm-10">
+        <div class="row">
+          <div class="col col-md-6 d-grid gap-2">
+          </div>
+          <div class="col col-md-6 d-grid gap-2">
+            <button type="submit" name="tambahfile" class="btn btn-success btn-block">
+              <span>Tambah Data</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+</html>
---
+++ user/admin/santri/data-santri/action/tambah/with-form.php
@@ -0,0 +1,188 @@
+<?php
+require_once('../../../../config.php');
+require_once('../../../akses.php');
+
+$getYear = date("Y");
+$getToday = date("Y-m-d");
+
+// form tambah data
+if(isset($_POST['tambahform'])) {
+  // set Nomor Induk Santri
+  $queryCheck = "SELECT nis FROM santri WHERE nis LIKE '%$getYear%' ORDER BY nis DESC LIMIT 1";
+  $resultCheck = mysqli_query($conn, $queryCheck);
+  $dataCheck = mysqli_fetch_array($resultCheck, MYSQLI_ASSOC);
+  if(!empty($dataCheck['nis'])) {
+    $setNIS = $dataCheck['nis']+1;
+  } else {
+    $setNIS = $getYear."001";
+  }
+
+  // get data from form
+  $namaLengkap = addslashes($_POST['namaLengkap']);
+  $panggilan = addslashes(ucwords($_POST['panggilan']));
+  $tempatLahir = addslashes(ucwords($_POST['tempat']));
+  $tglLahir = $_POST['tanggal'];
+  $tglLahir = formatTanggal($tglLahir);
+  $jenjangSekolah = $_POST['jenjangSekolah'];
+  $kelas = $_POST['kelas'];
+  // wali section
+  $namaBapak = addslashes($_POST['namaBapak']);
+  $pekerjaanBapak = addslashes($_POST['pekerjaanBapak']);
+  $namaIbu = addslashes($_POST['namaIbu']);
+  $pekerjaanIbu = addslashes($_POST['pekerjaanIbu']);
+  $telpWali = $_POST['telpWali'];
+  $alamat = addslashes($_POST['alamat']);
+  $infakBulanan = $_POST['infak'];
+  
+  $query = "INSERT INTO santri(`nis`, `nama_lengkap`, `panggilan`, `tempat_lahir`, `tgl_lahir`, `jenjang_sekolah`, `kelas`,
+            `nama_bapak`, `pekerjaan_bapak`,`nama_ibu`, `pekerjaan_ibu`, `no_telp_ortu`, `alamat_ortu`, `infak_bulanan`, `status`, `tgl_daftar`)
+            VALUES ('$setNIS', '$namaLengkap', '$panggilan', '$tempatLahir', '$tglLahir', '$jenjangSekolah', '$kelas',
+            '$namaBapak', '$pekerjaanBapak','$namaIbu', '$pekerjaanIbu', '$telpWali', '$alamat', '$infakBulanan', 'AKTIF', '$getToday')";
+  $result = mysqli_query($conn, $query);
+  
+  echo("<script>location.href = '/user/admin/santri/data-santri/santri.php';</script>");
+}
+
+// function for date formatting
+function formatTanggal($date){
+  return date('Y-m-d', strtotime($date));
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <!-- form input -->
+  <form method="post">
+
+  <!-- Nama -->
+  <div class="form-group row">
+    <label for="namaLengkap" class="col-sm-2 col-form-label">Nama Lengkap</label>
+    <div class="col-sm-10">
+      <input type="text" name="namaLengkap" class="form-control" id="namaLengkap" required>
+    </div>
+  </div><br>
+
+  <!-- Nama Panggilan -->
+  <div class="form-group row">
+    <label for="panggilan" class="col-sm-2 col-form-label">Nama Panggilan</label>
+    <div class="col-sm-10">
+      <input type="text" name="panggilan" class="form-control" id="panggilan" required>
+    </div>
+  </div><br>
+
+  <!-- Tempat, Tanggal Lahir -->
+  <div class="form-group row">
+    <label for="lahir" class="col-sm-2 col-form-label">Tempat, Tanggal Lahir</label>
+    <div class="col-sm-10">
+      <div class="input-group has-validation">
+        <input type="text" name="tempat" class="form-control" id="tempat" required>
+        <span class="input-group-text" id="inputGroupPrepend3">,</span>
+        <input type="date" name="tanggal" class="form-control" id="tanggal" required>
+      </div>
+    </div>
+  </div><br>
+
+  <!-- Jenjang Sekolah -->
+  <div class="form-group row">
+    <label for="jenjangSekolah" class="col-sm-2 col-form-label">Jenjang Sekolah</label>
+    <div class="col-sm-10">
+      <select class="form-select" name="jenjangSekolah" id="jenjangSekolah" required>
+        <option value="" selected disabled>Pilih Jenjang Sekolah</option>
+        <option value="PAUD/PRA TK">PAUD/PRA TK</option>
+        <option value="TK/RA">TK/RA</option>
+        <option value="SD/MI">SD/MI</option>
+        <option value="SMP/MTS">SMP/MTS</option>
+        <option value="SMA/MA">SMA/MA</option>
+      </select>
+    </div>
+  </div><br>
+
+  <!-- Kelas -->
+  <div class="form-group row">
+    <label for="kelas" class="col-sm-2 col-form-label">Kelas</label>
+    <div class="col-sm-10">
+      <input type="number" name="kelas" class="form-control" id="kelas" required>
+    </div>
+  </div><br>
+
+  <!-- Wali -->
+  <!-- Nama Bapak -->
+  <div class="form-group row">
+    <label for="namaBapak" class="col-sm-2 col-form-label">Nama Bapak</label>
+    <div class="col-sm-10">
+      <input type="text" name="namaBapak" class="form-control" id="namaBapak" required>
+    </div>
+  </div><br>
+
+  <!-- Pekerjaan Bapak -->
+  <div class="form-group row">
+    <label for="pekerjaanBapak" class="col-sm-2 col-form-label">Pekerjaan Bapak</label>
+    <div class="col-sm-10">
+      <input type="text" name="pekerjaanBapak" class="form-control" id="pekerjaanBapak" required>
+    </div>
+  </div><br>
+
+  <!-- Nama Ibu -->
+  <div class="form-group row">
+    <label for="namaIbu" class="col-sm-2 col-form-label">Nama Ibu</label>
+    <div class="col-sm-10">
+      <input type="text" name="namaIbu" class="form-control" id="namaIbu" required>
+    </div>
+  </div><br>
+
+  <!-- Pekerjaan Ibu -->
+  <div class="form-group row">
+    <label for="pekerjaanIbu" class="col-sm-2 col-form-label">Pekerjaan Ibu</label>
+    <div class="col-sm-10">
+      <input type="text" name="pekerjaanIbu" class="form-control" id="pekerjaanIbu" required>
+    </div>
+  </div><br>
+
+  <!-- Telp Wali -->
+  <div class="form-group row">
+    <label for="telpWali" class="col-sm-2 col-form-label">Telepon Wali</label>
+    <div class="col-sm-10">
+      <input type="number" name="telpWali" class="form-control" id="telpWali" required>
+    </div>
+  </div><br>
+
+  <!-- Alamat -->
+  <div class="form-group row">
+    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+    <div class="col-sm-10">
+      <textarea name="alamat" class="form-control" id="alamat" maxlength="255" rows="2" required></textarea>
+    </div>
+  </div><br>
+
+  <!-- Infak Bulanan -->
+  <div class="form-group row">
+    <label for="infak" class="col-sm-2 col-form-label">Infak Bulanan</label>
+    <div class="col-sm-10">
+      <select class="form-select" name="infak" id="infak" required>
+        <option value="" selected disabled>Pilih Infak Bulanan</option>
+        <option value="50000">Rp 50.000</option>
+        <option value="70000">Rp 70.000</option>
+      </select>
+    </div>
+  </div><br>
+
+  <!-- Button -->
+  <div class="form-group row">
+    <label for="button" class="col-sm-2 col-form-label"></label>
+    <div class="col-sm-10">
+      <div class="row">
+        <div class="col col-md-6 d-grid gap-2">
+          <button type="reset" class="btn btn-danger btn-block">
+            <span>Reset Data</span>
+          </button>
+        </div>
+        <div class="col col-md-6 d-grid gap-2">
+          <button type="submit" name="tambahform" class="btn btn-success btn-block">
+            <span>Tambah Data</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  </form>
+</html>
