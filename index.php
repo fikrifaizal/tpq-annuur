fddc53467d366ec65809f0213cecf54b532bacd5
fikrifaizal <fikrifaizal40@gmail.com>
2022-08-19 21:48:14 +0700

    fix bug

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
-if(isset($_COOKIE['id']) && isset($_COOKIE['role'])) {
-  switch($_COOKIE["role"]) {
+session_start();
+
+if(isset($_SESSION['id']) && isset($_SESSION['role'])) {
+  switch($_SESSION['role']) {
     case "ADMINISTRATOR":
-      session_start();
-      $_SESSION["id"] = $_COOKIE['id'];
-      $_SESSION["nama"] = $_COOKIE['nama'];
-      $_SESSION["role"] = $_COOKIE['role'];
       header("location: user/admin/dashboard.php");
       break;
     case "BENDAHARA TPQ":
-      session_start();
-      $_SESSION["id"] = $_COOKIE['id'];
-      $_SESSION["nama"] = $_COOKIE['nama'];
-      $_SESSION["role"] = $_COOKIE['role'];
       header("location: user/bendahara/dashboard.php");
       break;
     // default:
